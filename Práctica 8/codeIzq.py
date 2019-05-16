08639c0cd8672a71b9b51304b3ac5a5cac3ab9c8
Abraham Manuel <AbrMa@users.noreply.github.com>
2019-05-16 12:25:27 -0500

    Add files via upload

---
+++ Práctica 8/codeIzq.py
@@ -0,0 +1,42 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens = ('a', 'b')
+
+t_a = r'a'
+t_b = r'b'
+
+def t_error(t):
+    print("Símbolo ilegal '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lex.lex()
+
+def p_S(p):
+    ''' S : a S
+          | b S
+          | empty '''
+    pass
+
+def p_empty(p):
+    ''' empty : '''
+    pass
+
+def p_error(p):
+    global s
+    if p:
+        print("Error de sintaxis en '%s'" % p.value)
+        print(s," no está en el lenguaje")
+    else:
+        print("Error de sintaxis en EOF")
+        print(s," no está en el lenguaje")
+yacc.yacc()
+
+while 1:
+    try:
+        s = input('> ')
+    except EOFError:
+        break
+    if not s: continue
+    t = yacc.parse(s)
+
